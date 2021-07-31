7579d55bbf6220c5425cb98f68241965c3183fb1
Sanjiv Mishra <sanjiv04mishra@gmail.com>
2021-07-31 18:15:55 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -18,3 +18,11 @@ provider "azurerm" {
   features {}
 }
 
+
+provider "azuread" {
+  # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider to be used
+  version = "=1.1.0"
+  # client_id     = "00000000-0000-0000-0000-000000000000"
+  # client_secret = var.client_secret
+  # tenant_id     = "10000000-2000-3000-4000-500000000000"
+}
